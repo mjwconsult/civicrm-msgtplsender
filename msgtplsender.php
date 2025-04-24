d1507017d6ad5a458adcd741429713da28f29935
Matthew Wire (MJW) <mjw@mjwconsult.co.uk>
2025-04-24 12:19:26 +0100

    Release 1.3

--- msgtplsender.php
+++ msgtplsender.php
@@ -30,55 +30,10 @@ function msgtplsender_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
- */
-function msgtplsender_civicrm_postInstall() {
-  _msgtplsender_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
- */
-function msgtplsender_civicrm_uninstall() {
-  _msgtplsender_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
  */
 function msgtplsender_civicrm_enable() {
   _msgtplsender_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
- */
-function msgtplsender_civicrm_disable() {
-  _msgtplsender_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
- */
-function msgtplsender_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _msgtplsender_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
- */
-function msgtplsender_civicrm_entityTypes(&$entityTypes) {
-  _msgtplsender_civix_civicrm_entityTypes($entityTypes);
 }
